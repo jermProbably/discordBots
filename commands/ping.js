c6040a58e81a1c92de4a23ca2221098794ae256e
jermProbably <67804517+jermProbably@users.noreply.github.com>
2020-07-03 17:40:52 -0700

    Dev testing (#1)
    * Testing the commit
    
    * Testing Colors

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'ping',
   description: 'Ping!',
   execute(msg, args) {
-    msg.reply('pong');
-    msg.channel.send('pong');
+    msg.reply('Farts!')
+    // msg.channel.send('pong')
   },
-};
+}
