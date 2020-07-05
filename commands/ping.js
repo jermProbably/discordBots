f5cda03aaed028589d1fff1cdfca0ed9e410453e
Probably Jerm <github.jermz@odrallag.com>
2020-07-04 17:09:21 -0700

    Updating to work on iPad

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,14 @@
 module.exports = {
   name: 'ping',
   description: 'Ping!',
+
   execute(msg, args) {
-    msg.reply('Farts!')
-    // msg.channel.send('pong')
-  },
+    const userName = msg.author.username + '#' + msg.author.discriminator
+    // msg.reply('Pong!')
+    console.log(args)
+    if (args[0] === '1') {
+      return msg.channel.send('That\'s a number lol!')
+    }
+    msg.channel.send('Pong!')
+  }
 }
